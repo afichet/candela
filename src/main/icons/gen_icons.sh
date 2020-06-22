0b9bec8f334b2d4a11af494010891ad44d56ad6e
Alban Fichet <alban.fichet@gmx.fr>
2020-06-23 00:59:52 +0200

    Icons

---
+++ src/main/icons/gen_icons.sh
@@ -0,0 +1,21 @@
+convert icon.png -resize 16x16 base/16.png
+convert icon.png -resize 24x24 base/24.png
+convert icon.png -resize 32x32 base/32.png
+convert icon.png -resize 48x48 base/48.png
+convert icon.png -resize 64x64 base/64.png
+
+convert icon.png -resize 128x128 linux/128.png
+convert icon.png -resize 256x256 linux/256.png
+convert icon.png -resize 512x512 linux/512.png
+convert icon.png -resize 1024x1024 linux/1024.png
+
+
+convert icon.png -resize 128x128 mac/128.png
+convert icon.png -resize 256x256 mac/256.png
+convert icon.png -resize 512x512 mac/512.png
+convert icon.png -resize 1024x1024 mac/1024.png
+
+
+
+
+
